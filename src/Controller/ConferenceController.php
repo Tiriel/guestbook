76603d9b6acd48c83cc4f0109048f8e3e202901b
Benjamin ZASLAVSKY <benjamin.zaslavsky@sensiolabs.com>
2020-02-21 10:49:29 +0100

    Step 12 done

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -25,15 +25,13 @@ class ConferenceController extends AbstractController
      */
     public function index(ConferenceRepository $repository)
     {
-        return new Response($this->twig->render('conference/index.html.twig', [
-            'conferences' => $repository->findAll(),
-        ]));
+        return new Response($this->twig->render('conference/index.html.twig'));
     }
 
     /**
      * @Route("/conference/{id}", methods="GET", name="conference")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
